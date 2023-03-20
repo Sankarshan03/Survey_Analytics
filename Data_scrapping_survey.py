104ac0faba4443b1674f6c2bab61f9995c46a9be
Dank_1208 <109740128+Sankarshan03@users.noreply.github.com>
2023-03-21 00:01:40 +0530

    Update Data_scrapping_survey.py

--- Data_scrapping_survey.py
+++ Data_scrapping_survey.py
@@ -1,27 +1,53 @@
-
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-
+import csv
+#Function for extracting data from the website
 def get_data(str):
-    #Parsing data
+    #Parsing data into HTML
     response=requests.get(str)
     soup=BeautifulSoup(response.content,"html.parser")
-    Scrape=soup.find_all(class_="form_school_record_wrap")
-    dict={}
+    Scrape=soup.find_all("div",class_="form_school_record_wrap")
+    data=[]
     #Scraping data from website
     for district in Scrape:
-        dict['DISTRICT']=district.find_all("select",class_="form-control", id="district_code", name="district_code",onchange="district_load(this.value)")
-    for Block in Scrape:
-        dict['BLOCK']=Block.find_all("select",id="block_code" ,name="block_code", class_="form-control")
-    for School in Scrape:
-        dict['SCHOOL']=School.find_all("select",id="school_code", name="school_code", class_="form-control")
-    for Phase in Scrape:
-        dict['PHASE']=Phase.find_all("select", id="phase", name="phase", class_="form-control", onchange="phase_load(this.value)")
-    for Class in Scrape:
-        dict['CLASS']=Class.find_all("select", id="class_code", name="class_code" class_="form-control")
-        
+        dict1={}
+        dict1['DISTRICT']=district.find("div", class_="col-xs-15", id="district_div")
+        data.append(dict1)
+    
+    #for Block in Scrape:
+        #dict2={}
+        #dict2['BLOCK']=Block.find("select",id="block_code" ,name="block_code", class_="form-control")
+        #data.append(dict2)
+    #for School in Scrape:
+        #dict3={}
+        #dict3['SCHOOL']=School.find("select",id="school_code", name="school_code", class_="form-control")
+        #data.append(dict3)
+    #for Phase in Scrape:
+        #dict4={}
+        #dict4['PHASE']=Phase.find("select", id="phase", name="phase", class_="form-control", onchange="phase_load(this.value)")
+        #data.append(dict4)
+    #for Class in Scrape:
+        #dict5={}
+        #dict5['CLASS']=Class.find("select", id="class_code", name="class_code", class_="form-control")
+        #data.append(dict5)
+    return data
+    ###
+
+filename = 'district.csv'
+with open(filename, 'w', newline='') as f:
+    w = csv.DictWriter(f,['DISTRICT'])
+    w.writeheader()
+    str1=input()
+    data=get_data(str1)  
+    w.writerows(data)
+
+
+    
+
+
+
 
 
 
