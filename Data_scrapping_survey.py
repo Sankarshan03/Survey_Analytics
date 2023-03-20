c5cfe16b35a095d0a107cdf25f5e7a6b2c5aa867
Dank_1208 <109740128+Sankarshan03@users.noreply.github.com>
2023-03-20 23:17:38 +0530

    Update Data_scrapping_survey.py

--- Data_scrapping_survey.py
+++ Data_scrapping_survey.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -8,17 +9,20 @@ def get_data(str):
     response=requests.get(str)
     soup=BeautifulSoup(response.content,"html.parser")
     Scrape=soup.find_all(class_="form_school_record_wrap")
-
+    dict={}
     #Scraping data from website
     for district in Scrape:
-        dict={}
         dict['DISTRICT']=district.find_all("select",class_="form-control", id="district_code", name="district_code",onchange="district_load(this.value)")
     for Block in Scrape:
-        dict1={}
-        dict1['BLOCK']=Block.find_all("select",id="block_code" ,name="block_code", class_="form-control")
+        dict['BLOCK']=Block.find_all("select",id="block_code" ,name="block_code", class_="form-control")
     for School in Scrape:
-        dict2={}
-        dict2['SCHOOL']=School.find
-    Phase=
-    Class=
+        dict['SCHOOL']=School.find_all("select",id="school_code", name="school_code", class_="form-control")
+    for Phase in Scrape:
+        dict['PHASE']=Phase.find_all("select", id="phase", name="phase", class_="form-control", onchange="phase_load(this.value)")
+    for Class in Scrape:
+        dict['CLASS']=Class.find_all("select", id="class_code", name="class_code" class_="form-control")
+        
+
+
+
 
