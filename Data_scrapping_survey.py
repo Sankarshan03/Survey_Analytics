f02978b286c35db95055222562af18501ab90280
Dank_1208 <109740128+Sankarshan03@users.noreply.github.com>
2023-03-21 01:55:42 +0530

    Update Data_scrapping_survey.py

--- Data_scrapping_survey.py
+++ Data_scrapping_survey.py
@@ -5,16 +5,23 @@ import requests
 import csv
 #Function for extracting data from the website
 def get_data(str):
+    data1=[]
     #Parsing data into HTML
-    response=requests.get(str)
-    soup=BeautifulSoup(response.content,"html.parser")
-    Scrape=soup.find_all("div",class_="form_school_record_wrap")
-    data=[]
+
+    #try and catch block created for exception handling in case of connection timed out
+    try:
+        response=requests.get(str)
+        soup=BeautifulSoup(response.content,"html.parser",'lxml')
+        Scrape=soup.find_all("div",class_="form_school_record_wrap")
+        data=[]
     #Scraping data from website
-    for district in Scrape:
-        dict1={}
-        dict1['DISTRICT']=district.find("div", class_="col-xs-15", id="district_div")
-        data.append(dict1)
+        for district in Scrape:
+            dict1={}
+            dict1['DISTRICT']=district.find("div", class_="col-xs-15", id="district_div")
+            data.append(dict1)
+        data1.append(data)
+    except requests.exceptions.ConnectionError as e:
+        r = "No response"
     
     #for Block in Scrape:
         #dict2={}
@@ -32,8 +39,8 @@ def get_data(str):
         #dict5={}
         #dict5['CLASS']=Class.find("select", id="class_code", name="class_code", class_="form-control")
         #data.append(dict5)
-    return data
-    ###
+    return data1
+
 
 filename = 'district.csv'
 with open(filename, 'w', newline='') as f:
@@ -42,6 +49,19 @@ with open(filename, 'w', newline='') as f:
     str1=input()
     data=get_data(str1)  
     w.writerows(data)
+    df_new=pd.read_csv(filename)
+    excel = pd.ExcelWriter('district.xlsx')
+    df_new.to_excel(excel, index=False)
+    excel.save()
+
+    
+
+
+
+
+
+
+
 
 
     
